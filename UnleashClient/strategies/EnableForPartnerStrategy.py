48a504f8e496fbecbdb6252fec914defbd1148a2
parvez alam <alamparvez301@gmail.com>
2021-02-21 21:05:23 +0530

    Add new strategy to check whether feature is enabled for partner or not

---
+++ UnleashClient/strategies/EnableForPartnerStrategy.py
@@ -0,0 +1,22 @@
+from UnleashClient.strategies import Strategy
+
+class EnableForBusinesses(Strategy):
+    def load_provisioning(self) -> list:
+        return [
+            x.strip() for x in self.parameters["partnerNames"].split(',')
+        ]
+
+    def apply(self, context: dict = None) -> bool:
+        """
+        Check if feature is enabled for given partner or not
+        
+        Args:
+            context(dict): partner name provided as context
+        """
+        default_value = False
+
+        if "partnerNames" in context.keys():
+            default_value = context["partnerNames"] in self.parsed_provisioning
+
+        return default_value
+
