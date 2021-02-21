734aecbbb81b51ce9bfb2f2768549a3597cc5e39
parvez alam <alamparvez301@gmail.com>
2021-02-21 21:00:57 +0530

    Change domain check strategy

--- UnleashClient/strategies/EnableForDomainStrategy.py
+++ UnleashClient/strategies/EnableForDomainStrategy.py
@@ -2,7 +2,7 @@ from UnleashClient.strategies import Strategy
 
 class EnableForDomains(Strategy):
     def load_provisioning(self) -> list:
-        return [x.strip() for x in self.parameters["domainIds"].split(',')]
+        return [x.strip() for x in self.parameters["domainNames"].split(',')]
 
     def apply(self, context: dict = None) -> bool:
         """
@@ -12,7 +12,7 @@ class EnableForDomains(Strategy):
         """
         default_value = False
 
-        if "domainIds" in context.keys():
-            default_value = context["domainIds"] in self.parsed_provisioning
+        if "domainNames" in context.keys():
+            default_value = context["domainNames"] in self.parsed_provisioning
 
         return default_value
