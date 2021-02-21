fec6f2ae9e9f8330841a08ee644163593baf2c2e
Saumil Shah <saumil@haptik.ai>
2021-02-21 17:42:51 +0530

    init unleash client only during the first FF call

--- FeatureFlag/__init__.py
+++ FeatureFlag/__init__.py
@@ -2,33 +2,42 @@ class FeatureFlag:
     __instance = None
     __client = None
 
+    __url = None
+    __app_name = None
+    __redis_host = None
+    __redis_port = None
+    __redis_db = None
+
     def __init__(self):
         """ Virtually private constructor. """
-        if FeatureFlag.__instance is None or FeatureFlag.__client is None:
+        if FeatureFlag.__instance is None:
             raise Exception("FeatureFlag class not initialized!")
         else:
             return FeatureFlag.__instance
 
 
     @staticmethod
-    def get_unleash_client():
-        """ Static access method. """
-        if FeatureFlag.__instance is None or FeatureFlag.__client is None:
-            raise Exception("FeatureFlag class not initialized!")
-        return FeatureFlag.__client
-
-
-    @staticmethod
-    def init_instance(url: str, app_name: str, redis_host: str, redis_port: int, redis_db: int):
+    def __get_unleash_client():
         """ Static access method. """
         if FeatureFlag.__instance is None:
             FeatureFlag.__instance = FeatureFlag()
 
             from UnleashClient import UnleashClient
-            FeatureFlag.__client = UnleashClient(url, app_name, redis_host, redis_port, redis_db)
+            FeatureFlag.__client = UnleashClient(FeatureFlag.__url, FeatureFlag.__app_name, FeatureFlag.__redis_host,
+                                                 FeatureFlag.__redis_port, FeatureFlag.__redis_db)
             FeatureFlag.__client.initialize_client()
-        else:
-            raise Exception("FeatureFlag class already initialized!")
+
+        return FeatureFlag.__client
+
+
+    @staticmethod
+    def set_init_params(url: str, app_name: str, redis_host: str, redis_port: int, redis_db: int):
+        """ Static access method. """
+        FeatureFlag.__url = url
+        FeatureFlag.__app_name = app_name
+        FeatureFlag.__redis_host = redis_host
+        FeatureFlag.__redis_port = redis_port
+        FeatureFlag.__redis_db = redis_db
 
 
     @staticmethod
@@ -36,5 +45,5 @@ class FeatureFlag:
         """ Static access method. """
 
         context = {'domainIds': domain_ids}
-        variant = FeatureFlag.get_unleash_client().get_variant(feature_name, context)
+        variant = FeatureFlag.__get_unleash_client().get_variant(feature_name, context)
         return variant.enabled
