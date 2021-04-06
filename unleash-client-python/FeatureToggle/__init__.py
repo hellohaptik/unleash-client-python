d07e2446fe4d0556812b167d1c94c96dd5ba1d74
parvez alam <alamparvez301@gmail.com>
2021-04-06 15:00:01 +0530

    Refactor

--- unleash-client-python/FeatureToggle/__init__.py
+++ unleash-client-python/FeatureToggle/__init__.py
@@ -350,6 +350,10 @@ class FeatureToggles:
 
                                 # Keep updating this list for new strategies which gets added
 
+                        active_cas_env_name = f'{FeatureToggles.__cas_name}.
+                        {FeatureToggles.__environment}.'
+                        full_feature_name = full_feature_name.replace(active_cas_env_name, '')
+
                         # Assign the strategies data to feature name
                         response[full_feature_name]['partner_names'] = partner_names
                         response[full_feature_name]['business_via_names'] = business_via_names
